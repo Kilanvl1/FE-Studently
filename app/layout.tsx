162fdf977a7599e9401b0f5765a5adcee10710c1
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-05-03 21:39:27 +0200

    maximum scale set to 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
