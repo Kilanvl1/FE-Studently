96dca7f46acd256ba0bfda0b720deb265a5d01fd
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-05-22 16:02:33 +0200

    Wrapped in suspense boundary

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,11 +1,14 @@
 import { ResultsOverview } from "@/components/ui/ResultsOverview";
 import { Container } from "app/_components/Container";
+import { Suspense } from "react";
 
 export default function ResultsPage() {
   return (
     <section className="pt-16">
       <Container>
-        <ResultsOverview />
+        <Suspense>
+          <ResultsOverview />
+        </Suspense>
       </Container>
     </section>
   );
