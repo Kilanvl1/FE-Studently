de31667aa2a0baad9c9f21c8e939925edf5462b2
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-05-22 16:17:30 +0200

    Integrated calendly

---
+++ @/components/ui/Calendly.tsx
@@ -0,0 +1,19 @@
+import { Suspense, useEffect } from "react";
+
+export const Calendly = () => {
+  useEffect(() => {
+    const head = document.querySelector("head");
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    head.appendChild(script);
+  }, []);
+  return (
+    <Suspense>
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/studently-nl/consultation-meeting"
+        style={{ minWidth: "320px", height: "580px" }}
+      ></div>
+    </Suspense>
+  );
+};
