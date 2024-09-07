94e92d3de75516aedff4988559710b1339bf7ebc
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-09-07 21:51:49 +0200

    refactor: separate mobile and desktop landing pages

--- app/(landing-page)/LandingPage.tsx
+++ app/(landing-page)/LandingPage.tsx
@@ -6,25 +6,56 @@ import { LandingpageCTA } from "./LandingPageCTA";
 
 export const LandingPage = () => {
   return (
-    <section className="2xl:h-[840px] pt-32 2xl:p-0">
-      <div className="2xl:bg-hero bg-no-repeat bg-center flex flex-col gap-y-7 2xl:mt-10 2xl:h-[776px] items-center 2xl:items-start">
-        <Logo className="mt-8 ml-10 hidden 2xl:flex" />
-        <div className="flex flex-col gap-y-7 max-w-96 2xl:mt-24 2xl:ml-12">
-          <h3 className="font-bold text-3xl">
-            64% of international students miss out on government benefits.
-          </h3>
-          <BorderGradientForButton className="max-w-max">
-            <LandingpageCTA />
-          </BorderGradientForButton>
-        </div>
-        <Image
-          src="/mobile-banner.png"
-          alt="Banner"
-          className="relative bottom-[78px] 2xl:hidden"
-          width={350}
-          height={459}
-        />
+    <section>
+      <div className="block 2xl:hidden max-w-80 mx-auto pt-32">
+        <MobileLandingPage />
+      </div>
+      <div className="hidden 2xl:block h-[776px] mt-10 max-w-96">
+        <DesktopLandingPage />
       </div>
     </section>
+  );
+};
+
+const MobileLandingPage = () => {
+  return (
+    <>
+      <h3 className="font-bold text-3xl mb-7">
+        64% of international students miss out on government benefits.
+      </h3>
+      <BorderGradientForButton className="max-w-max">
+        <LandingpageCTA />
+      </BorderGradientForButton>
+      <Image
+        src="/mobile-banner.png"
+        alt="Banner"
+        className="relative bottom-[50px]"
+        width={350}
+        height={459}
+      />
+    </>
+  );
+};
+
+const DesktopLandingPage = () => {
+  return (
+    <>
+      <Image
+        src="/desktop-banner.png"
+        alt="desktop-banner"
+        width={1359}
+        height={776}
+        className="absolute -z-10"
+      />
+      <div className="relative top-8 left-8">
+        <Logo className="mb-24" />
+        <h3 className="font-bold text-3xl mb-7">
+          64% of international students miss out on government benefits.
+        </h3>
+        <BorderGradientForButton className="max-w-max">
+          <LandingpageCTA />
+        </BorderGradientForButton>
+      </div>
+    </>
   );
 };
